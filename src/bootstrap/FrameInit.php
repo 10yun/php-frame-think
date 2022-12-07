55e10241803741212673a3ebe4e498ab4e4111ee
ctocode <343196936@qq.com>
2022-12-07 18:17:26 +0800

    1

---
+++ src/bootstrap/FrameInit.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace shiyun\bootstrap;
+
+class FrameInit
+{
+    public static function postAutoloadDump()
+    {
+        $frame_path = preg_replace('/(\/|\\\\){1,}/', '/', __DIR__) . '/';
+        $rootPath =  dirname($frame_path, 5) . '/';
+
+        $configDiy = $rootPath . 'config' . DIRECTORY_SEPARATOR . 'shiyun' . DIRECTORY_SEPARATOR;
+        if (!is_dir($configDiy)) {
+            mkdir($configDiy, 0777, true);
+        }
+        $confCurrDir = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
+        $confFileArr = glob($confCurrDir . '*.php');
+        foreach ($confFileArr as $key => $val) {
+            $fileInfo = pathinfo($val);
+            $source = $val;
+            $target = $configDiy . $fileInfo['basename'];
+            if (is_file($target)) {
+                continue;
+            }
+            copy($source, $target);
+        }
+    }
+}
